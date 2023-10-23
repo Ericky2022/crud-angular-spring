1a276db920717cfdce3775a5eaea32f79272e19f
Ericky2022 <erivaldo.pereira.santana2016@gmail.com>
2023-10-22 23:06:45 -0300

    Criando Material table para Listar Cursos

--- crud-angular/src/app/app.module.ts
+++ crud-angular/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableDataSource } from '@angular/material/table';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
